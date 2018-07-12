d2280a605e6a25b9781ea9de96756f995a330bbd
thegreatsantini <lss13@my.fsu.edu>
2018-07-11 19:27:14 -0700

    added enymze config

--- jotto/src/Congrats.test.js
+++ jotto/src/Congrats.test.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdaptor from 'enzyme-adapter-react-16';
-
+import { shallow } from 'enzyme';
 import { findDataAttr, checkProps } from '../Test/testUtils';
 import Congrats from './Congrats';
 
-Enzyme.configure({ adapter: new EnzymeAdaptor() });
-
-const defaultProps = { success: false };
+const defaultProps = { success: false }
 
 /**
  * @function
