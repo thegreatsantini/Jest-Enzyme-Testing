31e3ef8e9bd333985c371da73f94584a4c438c5e
thegreatsantini <lss13@my.fsu.edu>
2018-07-31 17:59:11 -0700

    added tests for GuessedWorfs component

--- jotto/src/GuessedWords.test.js
+++ jotto/src/GuessedWords.test.js
@@ -1,43 +1,38 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+
 import { findDataAttr, checkProps } from '../Test/testUtils';
 import GuessedWords from './GuessedWords';
 
 const defaultProps = {
-        guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 }],
+  guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 }],
 };
-
 /**
- * @function
- * @param { Object } props -Component props specific to the congrats Component
- * @return { ShallowWrapper }
+* Factory function to create a ShallowWrapper for the GuessedWords component.
+* @function setup
+* @param {object} props - Component props specific to this setup.
+* @returns {ShallowWrapper}
 */
-
 const setup = (props = {}) => {
-    const setupProps = { ...defaultProps, ...props };
-    return shallow( <GuessedWords { ...setupProps } />);
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<GuessedWords {...setupProps} />)
 };
-
-test('test does not throw warning with expected props', () => {
-    // Check props takes in component and props to see if it renders
-    checkProps( GuessedWords, defaultProps );
+test('does not throw warning with expected props', () => {
+  checkProps(GuessedWords, defaultProps);
 });
-
 describe('if there are no words guessed', () => {
-  let wrapper;
-  beforeEach( ()=> {
+  let wrapper
+  beforeEach(() => {
     wrapper = setup({ guessedWords: [] });
   });
   test('renders without error', () => {
-    const component = findDataAttr( wrapper, 'component-guessed-words' );
+    const component = findDataAttr(wrapper, 'component-guessed-words');
     expect(component.length).toBe(1);
   });
-  test('renders insructions to guess a word', () => {
-    const insructions = findDataAttr( wrapper, 'guess-instructions' );
-    expect(insructions.text().length).not.toBe(0);
+  test('renders instructions to guess a word', () => {
+    const instructions = findDataAttr(wrapper, 'guess-instructions');
+    expect(instructions.text().length).not.toBe(0);
   });
 });
-
 describe('if there are words guessed', () => {
-
 });
